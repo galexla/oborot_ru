76a32e6388a1e12857f6ec3ce15235f77bdcdb92
Александр Гладких <versusnr@yandex.ru>
2023-03-13 19:26:01 +0400

    Add Garden class and its tests

--- src/Garden.php
+++ src/Garden.php
@@ -1,2 +1,14 @@
 <?php
 
+namespace OborotRu;
+
+
+class Garden
+{
+    private array $trees = [];
+
+    public function addTree(Tree $tree)
+    {
+        $this->trees[] = $tree;
+    }
+}
---
+++ tests/GardenTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use OborotRu\Garden;
+use OborotRu\AppleTree;
+use OborotRu\PearTree;
+
+
+class GardenTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function should_have_correct_trees_array_after_construct()
+    {
+        $garden = new Garden();
+        $trees = Helper::getHiddenProperty('OborotRu\\Garden', 'trees', $garden);
+        $this->assertIsArray($trees);
+        $this->assertEmpty($trees);
+    }
+
+    /**
+     * @test
+     */
+    public function should_add_trees()
+    {
+        $garden = new Garden();
+        $garden->addTree(new AppleTree(0));
+        $garden->addTree(new AppleTree(0));
+        $garden->addTree(new PearTree(0));
+
+        $trees = Helper::getHiddenProperty('OborotRu\\Garden', 'trees', $garden);
+        $nTrees = count($trees);
+
+        $this->assertEquals(3, $nTrees);
+
+        $this->assertInstanceOf(AppleTree::class, $trees[0]);
+        $this->assertInstanceOf(AppleTree::class, $trees[1]);
+        $this->assertInstanceOf(PearTree::class, $trees[2]);
+    }
+}
