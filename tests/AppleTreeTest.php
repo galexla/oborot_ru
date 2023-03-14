2f121c8cafdecee29c04fda1432b4f38c9592df8
Александр Гладких <versusnr@yandex.ru>
2023-03-14 14:16:05 +0400

    Add tests for AppleTree and PearTree classes

--- tests/AppleTreeTest.php
+++ tests/AppleTreeTest.php
@@ -12,12 +12,51 @@ class AppleTreeTest extends TestCase
     /**
      * @test
      */
-    public function should_have_correct_fruits_array_after_construct()
+    public function should_get_reg_number()
+    {
+        $regNumber = 12;
+        $tree = new AppleTree($regNumber);
+        $this->assertEquals($regNumber, $tree->getRegNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function should_get_min_max_fruits()
+    {
+        $tree = new AppleTree(0);
+        $minFruits = Helper::getHiddenProperty($tree, 'minFruits');
+        $maxFruits = Helper::getHiddenProperty($tree, 'maxFruits');
+        $this->assertEquals($minFruits, $tree->getMinFruits());
+        $this->assertEquals($maxFruits, $tree->getMaxFruits());
+    }
+
+    /**
+     * @test
+     */
+    public function should_init_fruits_array()
     {
         $tree = new AppleTree(0);
         $fruits = Helper::getHiddenProperty($tree, 'fruits');
+
         $this->assertIsArray($fruits);
         $this->assertEmpty($fruits);
+    }
+
+    /**
+     * @test
+     */
+    public function should_throw_if_incorrect_n_fruits()
+    {
+        $tree = new AppleTree(0);
+        $nFruits = $tree->getMinFruits() - 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $tree->grow($nFruits);
+
+        $nFruits = $tree->getMaxFruits() + 1;
+        $tree = new AppleTree(0);
+        $this->expectException(\InvalidArgumentException::class);
+        $tree->grow($nFruits);
     }
 
     /**
@@ -26,13 +65,18 @@ class AppleTreeTest extends TestCase
     public function should_grow_correct_amount_of_fruits()
     {
         $tree = new AppleTree(0);
+        $nFruits = $tree->getMinFruits();
+        $tree->grow($nFruits);
+        $fruits = Helper::getHiddenProperty($tree, 'fruits');
+
+        $this->assertEquals($nFruits, count($fruits));
+
+        $tree = new AppleTree(0);
         $tree->grow();
-        $minFruits = Helper::getHiddenProperty($tree, 'minFruits');
-        $maxFruits = Helper::getHiddenProperty($tree, 'maxFruits');
         $fruits = Helper::getHiddenProperty($tree, 'fruits');
         $nFruits = count($fruits);
 
-        $this->assertGreaterThanOrEqual($minFruits, $nFruits);
-        $this->assertLessThanOrEqual($maxFruits, $nFruits);
+        $this->assertGreaterThanOrEqual($tree->getMinFruits(), $nFruits);
+        $this->assertLessThanOrEqual($tree->getMaxFruits(), $nFruits);
     }
 }
--- tests/PearTreeTest.php
+++ tests/PearTreeTest.php
@@ -12,12 +12,51 @@ class PearTreeTest extends TestCase
     /**
      * @test
      */
-    public function should_have_correct_fruits_array_after_construct()
+    public function should_get_reg_number()
+    {
+        $regNumber = 12;
+        $tree = new PearTree($regNumber);
+        $this->assertEquals($regNumber, $tree->getRegNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function should_get_min_max_fruits()
+    {
+        $tree = new PearTree(0);
+        $minFruits = Helper::getHiddenProperty($tree, 'minFruits');
+        $maxFruits = Helper::getHiddenProperty($tree, 'maxFruits');
+        $this->assertEquals($minFruits, $tree->getMinFruits());
+        $this->assertEquals($maxFruits, $tree->getMaxFruits());
+    }
+
+    /**
+     * @test
+     */
+    public function should_init_fruits_array()
     {
         $tree = new PearTree(0);
         $fruits = Helper::getHiddenProperty($tree, 'fruits');
+
         $this->assertIsArray($fruits);
         $this->assertEmpty($fruits);
+    }
+
+    /**
+     * @test
+     */
+    public function should_throw_if_incorrect_n_fruits()
+    {
+        $tree = new PearTree(0);
+        $nFruits = $tree->getMinFruits() - 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $tree->grow($nFruits);
+
+        $nFruits = $tree->getMaxFruits() + 1;
+        $tree = new PearTree(0);
+        $this->expectException(\InvalidArgumentException::class);
+        $tree->grow($nFruits);
     }
 
     /**
@@ -26,13 +65,18 @@ class PearTreeTest extends TestCase
     public function should_grow_correct_amount_of_fruits()
     {
         $tree = new PearTree(0);
+        $nFruits = $tree->getMinFruits();
+        $tree->grow($nFruits);
+        $fruits = Helper::getHiddenProperty($tree, 'fruits');
+
+        $this->assertEquals($nFruits, count($fruits));
+
+        $tree = new PearTree(0);
         $tree->grow();
-        $minFruits = Helper::getHiddenProperty($tree, 'minFruits');
-        $maxFruits = Helper::getHiddenProperty($tree, 'maxFruits');
         $fruits = Helper::getHiddenProperty($tree, 'fruits');
         $nFruits = count($fruits);
 
-        $this->assertGreaterThanOrEqual($minFruits, $nFruits);
-        $this->assertLessThanOrEqual($maxFruits, $nFruits);
+        $this->assertGreaterThanOrEqual($tree->getMinFruits(), $nFruits);
+        $this->assertLessThanOrEqual($tree->getMaxFruits(), $nFruits);
     }
 }
