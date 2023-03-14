8c913a2e63234c414414ca75b9109db6da52ad0b
Александр Гладких <versusnr@yandex.ru>
2023-03-14 14:05:09 +0400

    Add tests for Apple and Pear classes

--- tests/AppleTest.php
+++ tests/AppleTest.php
@@ -12,7 +12,18 @@ class AppleTest extends TestCase
     /**
      * @test
      */
-    public function should_have_correct_weight()
+    public function should_set_weight()
+    {
+        $apple = new Apple(150);
+        $weight = $apple->getWeight();
+
+        $this->assertEquals($weight, $apple->getWeight());
+    }
+
+    /**
+     * @test
+     */
+    public function should_generate_correct_weight()
     {
         $apple = new Apple();
         $minWeight = Helper::getHiddenProperty($apple, 'minWeight');
@@ -22,5 +33,19 @@ class AppleTest extends TestCase
         $this->assertIsInt($weight);
         $this->assertGreaterThanOrEqual($minWeight, $weight);
         $this->assertLessThanOrEqual($maxWeight, $weight);
+    }
+
+    /**
+     * @test
+     */
+    public function should_throw_if_incorrect_weight()
+    {
+        $weight = 150 - 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $apple = new Apple($weight);
+
+        $weight = 180 + 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $apple = new Apple($weight);
     }
 }
--- tests/PearTest.php
+++ tests/PearTest.php
@@ -12,7 +12,18 @@ class PearTest extends TestCase
     /**
      * @test
      */
-    public function should_have_correct_weight()
+    public function should_set_correct_weight()
+    {
+        $pear = new Pear(150);
+        $weight = $pear->getWeight();
+
+        $this->assertEquals($weight, $pear->getWeight());
+    }
+
+    /**
+     * @test
+     */
+    public function should_generate_correct_weight()
     {
         $pear = new Pear();
         $minWeight = Helper::getHiddenProperty($pear, 'minWeight');
@@ -22,5 +33,19 @@ class PearTest extends TestCase
         $this->assertIsInt($weight);
         $this->assertGreaterThanOrEqual($minWeight, $weight);
         $this->assertLessThanOrEqual($maxWeight, $weight);
+    }
+
+    /**
+     * @test
+     */
+    public function should_throw_if_incorrect_weight()
+    {
+        $weight = 130 - 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $pear = new Pear($weight);
+
+        $weight = 170 + 1;
+        $this->expectException(\InvalidArgumentException::class);
+        $pear = new Pear($weight);
     }
 }
